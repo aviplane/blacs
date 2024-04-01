3d3403821f6e01f92ba16ec1976c98cdb776315d
aviplane <avikar96@gmail.com>
2024-04-01 09:42:55 -0700

    minor formatting?

--- blacs/remote.py
+++ blacs/remote.py
@@ -3,22 +3,25 @@ DEFAULT_PORT = 25227
 from labscript_utils.ls_zprocess import ZMQClient
 from labscript_utils.labconfig import LabConfig
 
+
 class Client(ZMQClient):
     """A ZMQClient for communication with blacs"""
 
     def __init__(self, host=None, port=None, timeout=None):
         ZMQClient.__init__(self)
         if host is None:
-            host = LabConfig().get('servers', 'blacs', fallback='localhost')
+            host = LabConfig().get("servers", "blacs", fallback="localhost")
         if port is None:
-            port = LabConfig().getint('ports', 'remote_blacs', fallback=DEFAULT_PORT)
+            port = LabConfig().getint("ports", "remote_blacs", fallback=DEFAULT_PORT)
         if timeout is None:
             timeout = LabConfig().getfloat(
-                'timeouts', 'communication_timeout', fallback=60
+                "timeouts", "communication_timeout", fallback=60
             )
         self.host = host
         self.port = port
         self.timeout = timeout
+        print("Test reload")
+        print("Blacs client here, ready to not work at all")
 
     def request(self, command, *args, **kwargs):
         return self.get(
@@ -27,21 +30,30 @@ class Client(ZMQClient):
 
     def say_hello(self):
         """Ping the blacs server for a response"""
-        return self.request('hello')
+        return self.request("hello")
 
     def get_n_shots(self):
         """Return the number of shots in the blacs queue"""
-        return self.request('get_n_shots')
+        # return 0
+        return self.request("get_n_shots")
 
     def device_states(self):
-        return self.request('device_states')
+        return self.request("device_states")
 
-    def restart_errored_devices(self, pineblaster = False, multidaq = False):
-        return self.request('restart_errored_devices', pineblaster = pineblaster, multidaq = multidaq)
+    def restart_errored_devices(self, pineblaster=False, multidaq=False):
+        return self.request(
+            "restart_errored_devices", pineblaster=pineblaster, multidaq=multidaq
+        )
 
     def start_queue(self):
         """If you need a docstring for this function idk what to tell you"""
-        return self.request('start_queue')
+        return self.request("start_queue")
 
     def top_file(self):
-        return self.request('top_file')
+        return self.request("top_file")
+
+    def queued_shots(self):
+        return self.request("queued_shots")
+
+
+# if __name__=="__main__":
